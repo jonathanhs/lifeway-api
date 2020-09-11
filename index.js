241b4e86330d5befaa820befd3b4b39ed2a73f25
Jonathan Hadisuryo <jonathan.hadisuryo@gmail.com>
2020-09-11 19:03:59 +0000

    added main app driver

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const jsonParser = require('body-parser').json;
+const routes = require('./routes/routes');
+
+// middleware
+app.use(jsonParser());
+app.use('/api/v0', routes);
+
+// listening port
+app.listen(port, () => {
+    console.log(`Lifeway API listening at http://localhost:${port}`)
+})
+
+module.exports = app;
